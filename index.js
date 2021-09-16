b8a640be2fd720e409d564e727092532dc48b1c8
nagaraj4128 <52693954+nagaraj4128@users.noreply.github.com>
2021-09-16 16:07:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const app = express();
 const path = require('path')
 const Patient = require('./models/patient')
 const methodOverride = require('method-override')
+const bodyParser=require('body-parser')
+var fs = require('fs');
 
+app.use(bodyParser.json());
+
+var myCss = {
+         style : fs.readFileSync('views/test.css','utf8')
+};
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/views'));
 
 mongoose.connect('mongodb://localhost:27017/hospital',{
   useNewUrlParser : true,
@@ -49,21 +57,39 @@ app.get('/showpatientdetails/:id',(req,res) =>{
       res.render('displaypatient',{person});
     }
   });
-})
+});
+
+app.post('/addappointment',(req,res)=>{
+    console.log(req.body);
+});
 
 app.post('/showsearchresults',async (req,res) =>{
   const name = req.body.patient.name;
-  console.log(name);
+  //console.log(name);
   var thename = name;
   const names = await Patient.find( { name : { $regex : new RegExp(thename, 'i') } } );
   res.render('displayresults.ejs',{names});
+});
+app.get('/test',(req,res)=>{
+  res.render('test',{
+    title: 'idc',
+    myCss: myCss
+  });
 })
-
 app.get('/display', async (req,res)=>{
   const patients_list = await Patient.find({});
   res.render('all.ejs',{patients_list});
 });
- 
+
+app.post('/addmed',async (req,res)=>{
+  const medname=req.body;
+  console.log(req.body);
+})
+
+app.get('/med', async (req,res)=>{
+  res.render('test.ejs');
+})
+
 app.listen(3000, ()=>{
   console.log("Server on port 3000");
 })
