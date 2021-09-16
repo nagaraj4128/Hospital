cd0664d81888310fe2de867e65b92615d7fe78dd
nagaraj4128 <52693954+nagaraj4128@users.noreply.github.com>
2021-09-16 16:07:31 +0530

    Add files via upload

--- Test/test.js
+++ Test/test.js
@@ -1,32 +1,36 @@
-// const { format } = require("express/lib/response");
-// const mongoose = require("mongoose");
-// const Patient = require("./models/patient");
-
-// mongoose.connect("mongodb://localhost:27017/hospital", {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function () {
-//   // we're connected!
-// });
-
-const text = document.getElementById("input");
+const text = document.getElementById("name");
+const quantity = document.getElementById("quantity");
+const date = document.getElementById("date");
+const impression = document.getElementById("imp");
 const button = document.getElementById("add");
+const submit = document.getElementById("submit");
 var ul = document.querySelector("ul");
-const v = {};
+var arr = [];
 button.addEventListener("click", async (e) => {
   e.preventDefault();
-  const textval = text.value;
+  const medicine = text.value; // med name
+  const quant = quantity.value; // quantity
+  console.log(medicine, quant)
+  let obj = { "med": medicine, "quantity": quant }
+  arr.push(obj);
   var li = document.createElement("li");
-  li.append(document.createTextNode(textval));
-  // console.log(textval);
-
+  li.append(document.createTextNode(medicine));
+  li.append(document.createTextNode(quant));
   ul.appendChild(li);
-  axios.post("http://localhost:3000/med", { sumit: "Sumit" });
-  // console.log(textval);
-  console.log("sent request");
+  //axios.post("http://localhost:3000/addmed",arr);
+  console.log(arr);
 });
+submit.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const date_text = date.value; // med name
+  const impvalue = impression.value; // quantity
+  let obj={"date":date_text,"impression":impvalue,"medcines":arr}
+  axios.post("http://localhost:3000/addappointment",obj)
+  .then(response => { 
+    console.log(response)
+  })
+  .catch(error => {
+      console.log(error.response)
+  });
+});
+
